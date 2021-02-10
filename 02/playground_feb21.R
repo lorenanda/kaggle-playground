b91a9747244b96a4f1e1a5f5b84038b94238b908
lorenanda <lorena.ciutacu@gmail.com>
2021-02-10 16:22:34 +0100

    start eda feb

--- 02/playground_feb21.R
+++ 02/playground_feb21.R
@@ -8,3 +8,6 @@ test <- fread("02/data/test.csv", data.table = F)
 head(test)
 
 skim(train)
+
+M_train <- cor(train %>% select(!id))
+corrplot(M_train, method = "circle", order = "hclust", addrect = 3)
