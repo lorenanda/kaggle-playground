211f44e620672284c9f8b6082c11c73d3874cbfd
lorenanda <lorena.ciutacu@gmail.com>
2021-02-12 18:02:09 +0100

    continue eda r

--- 02/playground_feb21.R
+++ 02/playground_feb21.R
@@ -11,14 +11,22 @@ head(train)
 test <- read_csv("Documents/kaggle-playground/02/data/test.csv")
 View(test)
 
-train_cat <- train
+train_cat <- train %>% select(2:11)
+View(train_cat)
+
+train_cont <- train %>% select(12:25)
+View(train_cont)
 
 ## EDA
+# distribution of the train features
 skim(train)
 
-ggcorrplot(test)
+ggcorrplot(train_cont)
 
 cor(train)
 
 M_train <- cor(train %>% select(!id))
 corrplot(M_train, method = "circle", order = "hclust", addrect = 3)
+
+##
+y_train <- train %>% pull(target)
